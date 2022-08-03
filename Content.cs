408865a4a64067335672aa88e569c7c540f3e939
GVR <gabriel7759@hotmail.com>
2022-08-02 21:40:10 -0700

    Initial Setup

---
+++ Content.cs
@@ -0,0 +1,9 @@
+namespace ContentAPI
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TypeId { get; set;  }
+    }
+}
---
+++ Controllers/ContentController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContentController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        { "Javascript", "Typescript", "Node.js", "Asp .Net", ".Net Core", "C#", "HTML", "CSS", "Git", "Apache" };
+
+        private readonly ILogger<ContentController> _logger;
+
+        public ContentController(ILogger<ContentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetSkills")]
+        public IEnumerable<Skill> Get()
+        {
+            return Enumerable.Range(1, 9).Select(index => new Skill
+            {
+                Id = index + 1,
+                Name = Summaries[index],
+                TypeId = index < 8 ? 1 : 2
+            }).ToArray();
+        }
+    }
+}
